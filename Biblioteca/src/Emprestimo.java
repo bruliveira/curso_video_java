205583cf689a6232f0ae803f3e2a932d073da494
klugaut.bruna.oliveira <bruna.oliveira@klugaut.com>
2023-01-26 19:18:39 -0300

    Organizando arquivos

--- Biblioteca/src/Emprestimo.java
+++
@@ -1,9 +0,0 @@
-public class Emprestimo {
-    private String usuario;
-    private String item;
-    private int qtdRenovado;
-    private String dataExpiracao;
-    private boolean status;
-    
-
-}
--- Biblioteca/src/Reserva.java
+++
@@ -1,7 +0,0 @@
-public class Reserva {
-    private String usuario;
-    private String livro;
-    private boolean status;
-
-
-}
